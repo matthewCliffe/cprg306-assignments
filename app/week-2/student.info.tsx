b341573dc1a4611be808c38068fa00e82e207ba8
kijuni <115662917+kijuni@users.noreply.github.com>
2026-01-28 15:35:21 -0700

    Assignment 2
    Assignment 2

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Name: Matthew Cliffe</h1>
+            <p>
+                Github:
+                <Link href="https://github.com/matthewCliffe/cprg306-assignments"> matthewCliffe/cprg306-assignments</Link>
+            </p>
+        </div>
+    )
+}
