ca64a05372b64af31f839ea70cae0aa41376e8de
kijuni <115662917+kijuni@users.noreply.github.com>
2026-01-28 22:29:06 -0700

    Assignment 3 update
    Added a prop definition in item.tsx

--- app/week-3/item.tsx
+++ app/week-3/item.tsx
@@ -1,5 +1,11 @@
 
-export default function Item({name, quantity, category}){
+type ItemProps = {
+  name: string;
+  quantity: number;
+  category: string;
+};
+
+export default function Item({name, quantity, category}: ItemProps){
     return(
         <main className= "border-2 w-1/4 p-2 mb-3 mx-auto">
             <ul>
